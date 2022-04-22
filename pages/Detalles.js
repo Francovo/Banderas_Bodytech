3982a46527a283f13a17525e0285df28417e8863
Franco <francovelasco10hotmail.com>
2022-04-22 16:49:47 -0500

    Falta detalles y filtro continentes

---
+++ pages/Detalles.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Detalles = ({datos}) => {
+    console.log(datos)
+
+  return (
+    <div>
+        UwU
+    </div>
+  )
+}
+
+export default Detalles
--- pages/index.js
+++ pages/index.js
@@ -10,35 +10,40 @@ import {
 import { React, useState, useEffect } from "react";
 import axios from "axios";
 import { BsFillSunFill, BsCloudMoonFill, BsSearch } from "react-icons/bs";
+import Detalles from "./Detalles";
+import Link from "next/link";
 
 const Home = () => {
   const [data, setData] = useState([]);
   const { colorMode, toggleColorMode } = useColorMode();
   const color = useColorModeValue("black", "black");
-  const [busqueda, setBusqueda] = useState([]);
-  const [dataFiltrada, setdataFiltrada] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [continente, setContinente] = useState("");
+  // const [idPais, setidPais] = useState('');
 
-  const handleChange = (e) => {
-    e.preventDefault()
-    console.log('La busqueda es:' + e.target.value)
-    setBusqueda(e.target.value)
-  }
+  //Handle Change del pais a ingresar (Input)
+  const handleChangeInput = (e) => {
+    setBusqueda(e.target.value);
+  };
+
+  //Handle Change del continente del Option
+  const handleChangeContinente = (e) => {
+    setContinente(e.target.value);
+    console.log(continente);
+  };
 
   useEffect(() => {
     axios
-      .get("https://restcountries.com/v2/all")
+      .get(
+        `https://restcountries.com/v2/${busqueda ? `name/${busqueda}` : "all"}`
+      )
       .then((response) => {
         setData(response.data);
-        console.log(response.data);
       })
       .catch((error) => {
         alert("La peticion no tuvo exito", error);
       });
-  }, []);
-  
-    useEffect(() => {
-      
-    }, [busqueda]);
+  }, [busqueda]);
 
   return (
     <Box>
@@ -61,7 +66,7 @@ const Home = () => {
       <Box padding="1rem" display="flex" justifyContent="space-between">
         <Box width="50%" display="flex" alignItems="center">
           <Button backgroundColor="white" borderRadius="10px 0px 0 10px">
-            <BsSearch color={color}/>
+            <BsSearch color={color} />
           </Button>
           <Input
             borderRadius="0 10px 10px 0"
@@ -71,13 +76,17 @@ const Home = () => {
             border="none"
             padding="1rem 0"
             value={busqueda}
-            onChange={handleChange}
+            onChange={handleChangeInput}
           />
         </Box>
-        <Select width="fit-content" backgroundColor="white" color={color}>
-          <option value="Selecciona un Continente" selected>
-            Continente
-          </option>
+        <Select
+          width="fit-content"
+          backgroundColor="white"
+          color={color}
+          onChange={handleChangeContinente}
+        >
+          <option value="Continente">Continente</option>
+          <option value="Africa">Africa</option>
           <option value="Antartida">Antartida</option>
           <option value="America">America</option>
           <option value="Asia">Asia</option>
@@ -88,46 +97,62 @@ const Home = () => {
 
       <Box
         display="grid"
-        gridTemplateColumns={{ base: "100%",sm:'50% 50%' , md: "25% 25% 25% 25%" }}
+        gridTemplateColumns={{
+          base: "100%",
+          sm: "50% 50%",
+          md: "25% 25% 25% 25%",
+        }}
         gap="1.5rem"
         padding="2rem"
       >
-        {data.map((paises) => (
+        {data.map((paises, index) => (
           <Box
-            key={paises}
+            key={`pais-${index}`}
             boxShadow="dark-lg"
             width="fit-content"
             backgroundColor="transparent"
           >
             <Img src={paises.flag} width="15rem" height="10rem" />
-
+            {/* Se utiliza codigo numerico del pais en el array como key del button */}
             <Button
-              key={paises.name}
+              key={paises.id}
               width="100%"
               height="fit-content"
               display="flex"
               flexDirection="column"
+              onClick={() => {
+                <Detalles datos={data} />;
+              }}
             >
-              <Box width="100%" align="left" padding="0.5rem 1rem 1rem 1rem">
-                <Box margin="0 0 1rem 0" backgroundColor='transparent'>
-                  <mark>{paises.name}</mark>
-                </Box>
+              <Link href="/Detalles">
+                <a>
+                  {" "}
+                  <Box
+                    width="100%"
+                    align="left"
+                    padding="0.5rem 1rem 1rem 1rem"
+                  >
+                    <Box margin="0 0 1rem 0" backgroundColor="transparent">
+                      <mark>{paises.name}</mark>
+                    </Box>
 
-                <Box fontSize="sm" backgroundColor='transparent'> 
-                  <Box>
-                    <mark>Poblacion: </mark>
-                    {paises.population}
-                  </Box>
-                  <Box>
-                    <mark>Region: </mark>
-                    {paises.region}
-                  </Box>
-                  <Box>
-                    <mark>Capital: </mark>
-                    {paises.capital}
-                  </Box>
-                </Box>
-              </Box>
+                    <Box fontSize="sm" backgroundColor="transparent">
+                      <Box>
+                        <mark>Poblacion: </mark>
+                        {paises.population}
+                      </Box>
+                      <Box>
+                        <mark>Region: </mark>
+                        {paises.region}
+                      </Box>
+                      <Box>
+                        <mark>Capital: </mark>
+                        {paises.capital}
+                      </Box>
+                    </Box>
+                  </Box>{" "}
+                </a>
+              </Link>
             </Button>
           </Box>
         ))}
